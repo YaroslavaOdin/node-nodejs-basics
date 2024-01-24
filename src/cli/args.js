c47ce4c7f4efe2a3fe6fb3fba6e65827a59d586c
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-01-24 19:24:24 +0300

    feat: add command line interface(src/cli) functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argv = process.argv;
+    for (let i = 0; i < argv.length; i++) {
+        if (argv[i].startsWith('--')) { 
+            console.log(`${argv[i].slice(2)} is ${argv[i + 1]},`);
+        }
+    }
 };
 
 parseArgs();
