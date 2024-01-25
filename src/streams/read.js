cbde40aa79cb33ba4610f2fceb69e733be8aa175
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-01-25 17:35:57 +0300

    feat: add streams (src/streams) functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(`${__dirname}/files/fileToRead.txt`);
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
