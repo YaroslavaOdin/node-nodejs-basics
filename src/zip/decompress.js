1c2bc68783465830c2ac4aa6af46f132ab89a796
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-01-26 12:06:05 +0300

    feat: add hash (src/hash) and zlib (src/zip) functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs'
+import zlib from 'node:zlib'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const source = fs.createReadStream(`${__dirname}/files/archive.gz`);
+    const destination = fs.createWriteStream(`${__dirname}/files/fileToCompress.txt`);
+    const unzip = zlib.createUnzip();
+    source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
