a393ad8b5ad1165caae3711e2e5b0ba0d387e168
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-01-26 18:12:00 +0300

    feat: add worker threads (src/wt) and child processes (src/cp) functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import child_process from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToScript = `${__dirname}/files/script.js`;
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = child_process.fork(pathToScript, args, { silent: true });
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const numOfCpus = os.cpus().length;
+const pathToWorker = `${__dirname}/worker.js`;
+
+function runWorker(index) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(pathToWorker, {
+            workerData: index,
+        });
+        worker.on('message', (message) => {
+            resolve(message);
+        });
+        worker.on('error', () => {
+            reject({ status: 'error', data: null });
+        });
+    });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+    for(let i = 0; i < numOfCpus; i++) {
+        promises.push(runWorker(i + 10));
+    }
+
+    const result = await Promise.all(promises);
+    console.log(result);
 };
 
 await performCalculations();
